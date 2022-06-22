92cc1b241f97d2353ef98b47adb86fddec36c324
TH-Jeon <89818381+TH-Jeon@users.noreply.github.com>
2022-06-22 14:03:33 -0400

    converted server to express

--- Week3/server.ts
+++ Week3/server.ts
@@ -1,44 +1,18 @@
-//require built-in modules
-import http from 'http';   //ECMAScript Module Pattern (ESM)
-import fs from 'fs';   //fs = file system
-import mime from 'mime-types';
+import express from 'express';
 
-let lookup = mime.lookup;   //alias for mime.lookup
-
+const app = express(); //creates a new express application
 const port = 3000;
 
-//when we create a server instance, it is IMMUTABLE = cannot be changed until the server is restarted
-//When the server is instantiated(created), it is IMMUTABLE
-const server = http.createServer(function(req, res) //req = request, res = response    
+app.get('/', function(req, res)
+//app.get('/', (req, res) => // '/' means default route for my application
 {
-    let path = req.url as string; //alias for req.url
-
-    if(path =="/" || path =="/home")
-    {
-        path = "/index.html";
-    }
-
-    let mime_type = lookup(path.substring(1)) as string;  //1 -> 2nd letter in path = i
-    console.log(`MIME TYPE: ${mime_type}`);
-
-    //Reads a file from the file system
-    fs.readFile(__dirname + path, function(err, data)
-    {
-        //Some error exists with the url
-        if(err)
-        {
-            res.writeHead(404); //status - file not found = file does not exist
-            //console.log(`ERROR: ${err.message}`);   //output to the dev console
-            return res.end("ERROR: 404 - File Not Found");   //output the error message to the page
-        }
-        //else = no error
-        res.setHeader("X-Content-Type-Options", "nosniff"); //security guard
-        res.writeHead(200, {'Content-Type': mime_type}); //status - all ok
-        return res.end(data);   //console.log(`DATA: ${data}`); --> output the file that was read to the page
-    });
+  res.send('Hello, World!');
 });
 
-server.listen(port, function() 
+//listener - similar to server.listen
+app.listen(port, function()
+//app.listen(port, () => 
 {
-    console.log(`Server running at Port: ${port}`);
-}); 
+  console.log(`Example app listening on port ${port}`);
+});
+
