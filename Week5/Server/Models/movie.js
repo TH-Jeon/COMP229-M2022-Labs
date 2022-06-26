4b2fbc6178987b2a798ecb43bf3437794fba37ed
TH-Jeon <89818381+TH-Jeon@users.noreply.github.com>
2022-06-26 15:52:59 -0400

    added Movie Model

---
+++ Week5/Server/Models/movie.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const Schema = mongoose_1.default.Schema;
+const MovieSchema = new Schema({
+    Name: String,
+    Year: String,
+    Director: String,
+    Rating: String
+});
+//# sourceMappingURL=movie.js.map
